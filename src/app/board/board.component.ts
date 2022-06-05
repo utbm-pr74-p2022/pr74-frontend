e84292871b2eef75071fbe634c1adf4681c7716a
Joris-viennet <viennet.joris@gmail.com>
2022-06-05 11:51:34 +0200

    add drag & drop

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Condition } from '../types/condition.type';
+import { Task } from '../types/task.type';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent {
+
+  conditions: Condition[] = [new Condition(1, "A faire"), new Condition(2, "En cours"), new Condition(3, "Terminé")];
+
+  tasks: Task[] = [new Task(1, "create project", 1, [1, 5]), new Task(2, "create menu", 1, [2, 3, 9, 12]), new Task(3, "implement board", 1, [1, 7, 5])]
+
+  draggedTask: Task | undefined;
 
   constructor() { }
 
-  ngOnInit(): void {
+  dragStart(task: Task) {
+      this.draggedTask = task;
   }
 
+  drop(id: number) {
+      if (this.draggedTask) {
+        this.draggedTask.idCondition = id;
+        this.draggedTask = undefined;
+      }
+  }
+
+  dragEnd() {
+      this.draggedTask = undefined;
+  }
 }
