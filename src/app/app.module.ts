8b073ee7697aa4258841e15ccd2ffacaab909f31
Joris-viennet <viennet.joris@gmail.com>
2022-06-07 15:54:52 +0200

    correction bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { MenuComponent } from './menu/menu.component';
-
-import {MenuModule} from 'primeng/menu';
+import { MenuModule } from 'primeng/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
-import {CardModule} from 'primeng/card';
+import { CardModule } from 'primeng/card';
 import { BoardComponent } from './board/board.component';
 import { BacklogComponent } from './backlog/backlog.component';
 import { SettingsComponent } from './settings/settings.component';
-import {AvatarModule} from 'primeng/avatar';
-import {AvatarGroupModule} from 'primeng/avatargroup';
-import { ConnectionComponent } from './connection/connection.component';
-import {InputTextModule} from 'primeng/inputtext';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
-import {ToastModule} from 'primeng/toast';
-import { ProjectsComponent } from './projects/projects.component';
-import {ToolbarModule} from 'primeng/toolbar';
-import {TableModule} from 'primeng/table';
-
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { HttpClientModule } from '@angular/common/http';
-import {DialogModule} from 'primeng/dialog';
-import {MultiSelectModule} from 'primeng/multiselect';
-
+import { DialogModule } from 'primeng/dialog';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ConnectionComponent } from './connection/connection.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { DragDropModule } from 'primeng/dragdrop';
 
 @NgModule({
   declarations: [
@@ -59,6 +55,7 @@ import {MultiSelectModule} from 'primeng/multiselect';
     HttpClientModule,
     DialogModule,
     MultiSelectModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
